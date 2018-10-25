1689894c2a7bc9774e1ced50be856669f055b81a
Cody Laura Buffaloe <codylbuffaloe@gmail.com>
2018-10-24 17:54:14 -0700

    Test script for all four GET requests

--- rest-tester/app/controllers/users_controller.rb
+++ rest-tester/app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def index
     @name = "I am the Index action!"
   end
+  def show
+    @name = "I am the Show action!"
+  end
+  def new
+    @name = "I am the New action!"
+  end
+  def edit
+    @name = "I'm the Edit action!"
+  end
 end
--- rest-tester/app/script/rest_requestor.rb
+++ rest-tester/app/script/rest_requestor.rb
@@ -1,5 +1,5 @@
 require 'rest-client'
 
-url = "http://localhost:3000/users"
+url = "http://localhost:3000/users/1/edit"
 
 puts RestClient.get(url)
